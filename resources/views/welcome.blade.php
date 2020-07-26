5e2df52b8431689d45d94fc61fd8616e8289a1e0
Eljeen Banta <eljeenbanta@gmail.com>
2020-07-26 15:23:47 +0100

    fixed user interface

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,48 +5,44 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-        <title>Laravel</title>
+        <title>Aphix</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
        
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
         <script src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        
     
         <!-- Styles -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        
-
     </head>
-    <body>
-        <div class = "container">
-            <div class="search-form-container">
-                    <form id="keywordForm" method="post" action="">
-                        <div class="input-row">
-                            <input class="input-field" type="search" id="keyword" name="keyword"  placeholder="Search YouTube videos.">
-                        </div>
-
-                        <button class="btn btn-success" id="submit">Search</button>
-                    </form>
-            </div>
-            <div class = "row">
-                <div id="search-result"></div>
-            </div>
-        </div>
-
    
+    <body>
+         <div class = "container">
+            <div class = "row">
+                <div class = "col-lg-6">   
+                    <form id="keywordForm" method="post" action="">
+                        <div class="input-group">
+                            <input type="text" class="form-control" id="keyword" placeholder="Search" name="search">
+                            <div class="input-group-btn">
+                                <button class="btn btn-default" type="submit"><i class="fas fa-search"></i></button>
+                            </div>
+                    </form>
+                </div>
+            </div>
+            <br><br>
+            <div id="search-result"></div>
+        </div>
+             
     </body>
     <script type="text/javascript">
         $('#keywordForm').on('submit',function(event){
             event.preventDefault();
-
-            keyword = $('#keyword').val();
-          
-            $.ajax({
-            //    cache: false,
-                
-                headers: {
+             keyword = $('#keyword').val();
+             $.ajax({
+                cache: false,
+                 headers: {
                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
                     }, 
                 type:'GET',
@@ -56,12 +52,12 @@
                     videoList = "";
                     $.each(data.items, function(index, e) {
                     videoList = videoList + 
-                                '<div class = "col-lg-12">' +
-                                '<a href=""><img class= "img-thumbnail" alt="' + e.snippet.title + '" src="' + e.snippet.thumbnails.default.url + '"></a>'+
-                                '<h2>' +e.snippet.title+'</h2>' + 
-                                '<p>' +e.snippet.description+'</p>' +
+                                '<div class = "flex-container"><div>' +
+                                '<a href="//www.youtube.com/embed/'+e.id.videoId+'" target="_blank"><img class= "img-thumbnail" style="min-width:200px" alt="' + e.snippet.title + '" src="' + e.snippet.thumbnails.default.url + '">'+
+                                '</div><div style="flex-grow: 8"><p><b><h2>' +e.snippet.title+'</b></h2><h5>' 
+                                 +e.snippet.description+'</h5></p></div></a>' +
                                 '</div>';
-                });
+                    });
                     $('#search-result').html(videoList);
                     console.log(data);
                 }
